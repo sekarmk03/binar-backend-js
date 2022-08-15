d804383daeae5516a33d00f505034f522d25139a
lilikwn <lilik1.wahyu7@gmail.com>
2022-08-15 20:46:05 +0700

    memperbaiki deklarasi variable

--- Chapter-2/latihan-d1/izaz_john_lilik_risma/kelilingLingkaran.js
+++ Chapter-2/latihan-d1/izaz_john_lilik_risma/kelilingLingkaran.js
@@ -5,6 +5,7 @@ const interface = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
+
 const input = (question) => {
   return new Promise(resolve => {
     interface.question(question, (data) => {
@@ -13,9 +14,11 @@ const input = (question) => {
   })
 }
 
+const pi = 3.14;
+const kelilingLingkaran = (r) => 2 * pi * r;
+
 const main = async() => {
   const radius = await input('Masukkan Radius : ');
-  const kelilingLingkaran = (radius) => 2 * 3.14 * radius;
   console.log(kelilingLingkaran(radius))
   interface.close()
 }
