d7f1c9a6931b4a2fca52e2ef3273b32c1ef2d905
Charles <charlesgunawan32@gmail.com>
2022-08-19 20:38:05 +0700

    Charles punya

---
+++ Chapter-2/topic-2/latihan-1/charles.js
@@ -0,0 +1,118 @@
+/*
+
+Output yang diinginkan :
+
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+
+*/
+
+function segitiga1(panjang) {
+    let hasil = '';
+    for (let i = 0; i<panjang; i++){
+        for (let j = 0; j<=i; j++){
+            hasil +='* '
+        }
+        hasil +='\n';
+    }
+    return hasil;
+}
+
+console.log(segitiga1(5));
+
+
+/*
+
+Output yang diinginkan :
+
+
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
+
+function segitiga2(panjang) {
+    let hasil = '';
+    for (let i = 0; i<panjang; i++){
+        for (let j = panjang-1; j>=i; j--){
+            hasil +='* '
+        }
+        hasil +='\n';
+    }
+    return hasil;
+}
+
+console.log(segitiga2(5));
+
+
+/*
+
+Output yang diinginkan :
+
+
+* * * * *
+  * * * *
+    * * *
+      * *
+        *
+
+*/
+
+function segitiga3(panjang) {
+    let hasil = '';
+    for (let i = panjang; i>0; i--){
+        for (let j = panjang; j>0; j--){
+            if(j>i){
+                hasil +='  '
+            }
+            else {
+                hasil +='* '
+            }
+        }
+        hasil +='\n';
+    }
+    return hasil;
+}
+
+console.log(segitiga3(5));
+
+
+/*
+
+Output yang diinginkan :
+
+
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+
+*/
+
+
+function segitiga4(panjang) {
+    let hasil = '';
+    for (let i = panjang; i>0; i--){
+        for (let j = 1; j<=panjang; j++){
+            if(j>=i){
+                hasil +='* '
+            }
+            else {
+                hasil +='  '
+            }
+        }
+        hasil +='\n';
+    }
+    return hasil;
+}
+
+console.log(segitiga4(5));
