d5b5c56b29ad0994fa2ba9e28e2adac6eaa6d53c
Doni Wahyu S <doniwahyu56@gmail.com>
2022-08-19 20:26:16 +0700

    upload

---
+++ Chapter-2/topic-2/latihan-1/coba.js
@@ -0,0 +1,10 @@
+let baris = 0
+for (baris = 0; baris <= 2 ; baris++) {
+ 
+    // memBuat sejumlah spasi
+    for (i = 1; i <= 0 - baris; i++) {
+    console.log(" "); // Karakter spasi
+     }
+for (i = 1; i < 2 * baris; i++) {
+		console.log("*"); }
+ }
