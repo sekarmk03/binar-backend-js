1143c49433e076772b60383608481d39183b173a
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2022-08-19 19:43:18 +0700

    Sekar done all function

--- Chapter-2/topic-2/latihan-1/sekar.js
+++ Chapter-2/topic-2/latihan-1/sekar.js
@@ -13,6 +13,12 @@ Output yang diinginkan :
 */
 
 function segitiga1(panjang) {
+  for (let i = 0; i < panjang; i++) {
+    for (let j = 0; j <= i; j++) {
+        process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+  }
 }
 
 console.log(segitiga1(5));
@@ -33,6 +39,12 @@ Output yang diinginkan :
 */
 
 function segitiga2(panjang) {
+  for (let i = 0; i < panjang; i++) {
+    for(let j = 0; j < panjang - i; j++) {
+        process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+  }
 }
 
 console.log(segitiga2(5));
@@ -53,6 +65,15 @@ Output yang diinginkan :
 */
 
 function segitiga3(panjang) {
+  for (let i = 0; i < panjang; i++) {
+    for (let j = 0; j < i; j++) {
+        process.stdout.write(" ");
+    }
+    for (let j = 0; j < panjang - i; j++) {
+        process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+  }
 }
 
 console.log(segitiga3(5));
@@ -73,6 +94,15 @@ Output yang diinginkan :
 */
 
 function segitiga4(panjang) {
+  for (let i = 0; i < panjang; i++) {
+    for (let j = 0; j < panjang - i - 1; j++) {
+        process.stdout.write(" ");
+    }
+    for (let j = 0; j <= i; j++) {
+        process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+  }
 }
 
 console.log(segitiga4(5));
