34cd148eee23504b4943dd0334c5af4d156c9b6a
Daniel Manurung <danielmanurung79@gmail.com>
2022-09-07 22:01:57 +0700

    menambahkan file latihan

---
+++ Chapter-3/latihan-2/daniel.sql
@@ -0,0 +1,42 @@
+create database youtube;
+
+create table users(
+    id int primary key not null,
+    name varchar(100) not null,
+    email varchar(50) not null,
+    password varchar(50) not null
+);
+
+create table channels(
+    id int primary key not null,
+    user_id int not null,
+    name varchar(100) not null,
+    description text
+);
+
+create table subscribers(
+    id int primary key not null,
+    channel_id int not null,
+    user_id int not null
+);
+
+create table videos(
+    id int primary key not null,
+    title varchar(100) not null,
+    description text,
+    channel_id int not null
+);
+
+create table comments(
+    id int primary key not null,
+    video_id int not null,
+    user_id int not null,
+    comment text 
+);
+
+create table likes(
+    id int primary key not null,
+    video_id int not null,
+    user_id int not null
+);
+
