c557fe826c86b99a0447c2f9fa66569d8f1e1b32
Daniel Manurung <danielmanurung79@gmail.com>
2022-09-07 22:25:32 +0700

    upload latihan

--- Chapter-3/latihan-2/daniel.sql
+++ Chapter-3/latihan-2/daniel.sql
@@ -1,5 +1,5 @@
-create table users(
-    id int primary key not null,
+CREATE TABLE users(
+    id bigserial primary key,
     name varchar(100) not null,
     email varchar(50) not null,
     password varchar(50) not null
