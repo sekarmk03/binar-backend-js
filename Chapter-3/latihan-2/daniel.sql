c15d088ab3456bb6e61be2daa2a8a06fc2f6db55
Daniel Manurung <danielmanurung79@gmail.com>
2022-09-07 22:32:00 +0700

    upload latihan

--- Chapter-3/latihan-2/daniel.sql
+++ Chapter-3/latihan-2/daniel.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users(
+create table users(
     id bigserial primary key,
     name varchar(100) not null,
     email varchar(50) not null,
@@ -6,34 +6,34 @@ CREATE TABLE users(
 );
 
 create table channels(
-    id int primary key not null,
+    id bigserial primary key,
     user_id int not null,
     name varchar(100) not null,
     description text
 );
 
 create table subscribers(
-    id int primary key not null,
+    id bigserial primary key,
     channel_id int not null,
     user_id int not null
 );
 
 create table videos(
-    id int primary key not null,
+    id bigserial primary key,
     title varchar(100) not null,
     description text,
     channel_id int not null
 );
 
 create table comments(
-    id int primary key not null,
+    id bigserial primary key,
     video_id int not null,
     user_id int not null,
     comment text 
 );
 
 create table likes(
-    id int primary key not null,
+    id bigserial primary key,
     video_id int not null,
     user_id int not null
 );
