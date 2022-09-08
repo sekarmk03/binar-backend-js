b6e16c408ec018cd8bd9e02b82f3c58a04e8c4e9
tatang <tatang.romadhona@rushowl.sg>
2022-09-08 20:41:19 +0800

    feat: add data sql

---
+++ Chapter-3/latihan-3/data.sql
@@ -0,0 +1,514 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.2
+-- Dumped by pg_dump version 14.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: channel_subscribers; Type: TABLE; Schema: public; Owner: tatangdev
+--
+
+CREATE TABLE public.channel_subscribers (
+    id bigint NOT NULL,
+    channel_id integer NOT NULL,
+    user_id integer NOT NULL
+);
+
+
+ALTER TABLE public.channel_subscribers OWNER TO tatangdev;
+
+--
+-- Name: channel_subscribers_id_seq; Type: SEQUENCE; Schema: public; Owner: tatangdev
+--
+
+CREATE SEQUENCE public.channel_subscribers_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.channel_subscribers_id_seq OWNER TO tatangdev;
+
+--
+-- Name: channel_subscribers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tatangdev
+--
+
+ALTER SEQUENCE public.channel_subscribers_id_seq OWNED BY public.channel_subscribers.id;
+
+
+--
+-- Name: channels; Type: TABLE; Schema: public; Owner: tatangdev
+--
+
+CREATE TABLE public.channels (
+    id bigint NOT NULL,
+    user_id integer NOT NULL,
+    name character varying(255) NOT NULL,
+    description text
+);
+
+
+ALTER TABLE public.channels OWNER TO tatangdev;
+
+--
+-- Name: channels_id_seq; Type: SEQUENCE; Schema: public; Owner: tatangdev
+--
+
+CREATE SEQUENCE public.channels_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.channels_id_seq OWNER TO tatangdev;
+
+--
+-- Name: channels_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tatangdev
+--
+
+ALTER SEQUENCE public.channels_id_seq OWNED BY public.channels.id;
+
+
+--
+-- Name: comments; Type: TABLE; Schema: public; Owner: tatangdev
+--
+
+CREATE TABLE public.comments (
+    id bigint NOT NULL,
+    video_id integer NOT NULL,
+    user_id integer NOT NULL,
+    comment text NOT NULL
+);
+
+
+ALTER TABLE public.comments OWNER TO tatangdev;
+
+--
+-- Name: comments_id_seq; Type: SEQUENCE; Schema: public; Owner: tatangdev
+--
+
+CREATE SEQUENCE public.comments_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.comments_id_seq OWNER TO tatangdev;
+
+--
+-- Name: comments_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tatangdev
+--
+
+ALTER SEQUENCE public.comments_id_seq OWNED BY public.comments.id;
+
+
+--
+-- Name: likes; Type: TABLE; Schema: public; Owner: tatangdev
+--
+
+CREATE TABLE public.likes (
+    id bigint NOT NULL,
+    video_id integer NOT NULL,
+    user_id integer NOT NULL
+);
+
+
+ALTER TABLE public.likes OWNER TO tatangdev;
+
+--
+-- Name: likes_id_seq; Type: SEQUENCE; Schema: public; Owner: tatangdev
+--
+
+CREATE SEQUENCE public.likes_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.likes_id_seq OWNER TO tatangdev;
+
+--
+-- Name: likes_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tatangdev
+--
+
+ALTER SEQUENCE public.likes_id_seq OWNED BY public.likes.id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: tatangdev
+--
+
+CREATE TABLE public.users (
+    id bigint NOT NULL,
+    name character varying(255) NOT NULL,
+    email character varying(255) NOT NULL,
+    password character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO tatangdev;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: tatangdev
+--
+
+CREATE SEQUENCE public.users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO tatangdev;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tatangdev
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: videos; Type: TABLE; Schema: public; Owner: tatangdev
+--
+
+CREATE TABLE public.videos (
+    id bigint NOT NULL,
+    title character varying(255) NOT NULL,
+    description text,
+    channel_id bigint NOT NULL
+);
+
+
+ALTER TABLE public.videos OWNER TO tatangdev;
+
+--
+-- Name: videos_id_seq; Type: SEQUENCE; Schema: public; Owner: tatangdev
+--
+
+CREATE SEQUENCE public.videos_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.videos_id_seq OWNER TO tatangdev;
+
+--
+-- Name: videos_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tatangdev
+--
+
+ALTER SEQUENCE public.videos_id_seq OWNED BY public.videos.id;
+
+
+--
+-- Name: channel_subscribers id; Type: DEFAULT; Schema: public; Owner: tatangdev
+--
+
+ALTER TABLE ONLY public.channel_subscribers ALTER COLUMN id SET DEFAULT nextval('public.channel_subscribers_id_seq'::regclass);
+
+
+--
+-- Name: channels id; Type: DEFAULT; Schema: public; Owner: tatangdev
+--
+
+ALTER TABLE ONLY public.channels ALTER COLUMN id SET DEFAULT nextval('public.channels_id_seq'::regclass);
+
+
+--
+-- Name: comments id; Type: DEFAULT; Schema: public; Owner: tatangdev
+--
+
+ALTER TABLE ONLY public.comments ALTER COLUMN id SET DEFAULT nextval('public.comments_id_seq'::regclass);
+
+
+--
+-- Name: likes id; Type: DEFAULT; Schema: public; Owner: tatangdev
+--
+
+ALTER TABLE ONLY public.likes ALTER COLUMN id SET DEFAULT nextval('public.likes_id_seq'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: tatangdev
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: videos id; Type: DEFAULT; Schema: public; Owner: tatangdev
+--
+
+ALTER TABLE ONLY public.videos ALTER COLUMN id SET DEFAULT nextval('public.videos_id_seq'::regclass);
+
+
+--
+-- Data for Name: channel_subscribers; Type: TABLE DATA; Schema: public; Owner: tatangdev
+--
+
+COPY public.channel_subscribers (id, channel_id, user_id) FROM stdin;
+1	1	3
+2	1	4
+3	3	4
+4	1	5
+5	2	5
+6	2	6
+7	3	6
+\.
+
+
+--
+-- Data for Name: channels; Type: TABLE DATA; Schema: public; Owner: tatangdev
+--
+
+COPY public.channels (id, user_id, name, description) FROM stdin;
+1	1	close the door podcast	podcast minta maap
+2	2	detective aldo	konten investigasi
+3	3	tim 2 one	cinematic video
+\.
+
+
+--
+-- Data for Name: comments; Type: TABLE DATA; Schema: public; Owner: tatangdev
+--
+
+COPY public.comments (id, video_id, user_id, comment) FROM stdin;
+1	5	1	incentive
+2	1	1	nonremittal
+3	3	1	manufacture
+4	4	1	arientation
+5	7	1	dark
+6	8	2	asylum
+7	4	2	mosque
+8	3	2	present
+9	9	2	bother
+10	1	2	live
+11	2	3	model
+12	3	3	piece
+13	4	3	net
+14	5	3	loyalty
+15	9	3	convenience
+16	7	4	ancestor
+17	9	4	rebellion
+18	3	4	diplomat
+19	1	4	slice
+20	4	4	take
+21	9	5	kitchen
+22	7	5	track
+23	3	5	torture
+24	4	5	observer
+25	5	5	estate
+26	2	6	weed
+27	9	6	motorcycle
+28	5	6	major
+29	3	6	advocate
+30	7	6	nominate
+\.
+
+
+--
+-- Data for Name: likes; Type: TABLE DATA; Schema: public; Owner: tatangdev
+--
+
+COPY public.likes (id, video_id, user_id) FROM stdin;
+37	7	1
+38	1	1
+39	5	1
+40	4	1
+41	2	1
+42	5	2
+43	8	2
+44	2	2
+45	6	2
+46	7	2
+47	2	3
+48	3	3
+49	5	3
+50	6	3
+51	4	3
+52	7	4
+53	3	4
+54	8	4
+55	5	4
+56	1	4
+57	8	5
+58	9	5
+59	2	5
+60	5	5
+61	4	5
+62	6	6
+63	8	6
+64	5	6
+65	3	6
+66	8	6
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: tatangdev
+--
+
+COPY public.users (id, name, email, password) FROM stdin;
+1	deddy corbuzier	deddy@gmail.com	password
+2	aldo ditario	aldo@gmail.com	password
+3	chandra liow	chandra@gmail.com	password
+4	tatang	tatang@mail.com	password
+5	irji	irji@mail.com	password
+6	fais	fais@mail.com	password
+\.
+
+
+--
+-- Data for Name: videos; Type: TABLE DATA; Schema: public; Owner: tatangdev
+--
+
+COPY public.videos (id, title, description, channel_id) FROM stdin;
+1	Somasi UUS	video uus minta maap	1
+2	Ukraina VS Rusia	penjelasan ahli tentang perang	1
+3	Bintang emon	ngalor ngidul bareng bintang emon	1
+4	Apakah alien ada?	benarkah alien exist di alam semesta?	2
+5	ada apa dengan binomskuy	penipuan terbesar	2
+6	benarkah bumi itu datar?	emang iya?	2
+7	Reaksi Editor Indonesia Part 1	reaksi blackpink	3
+8	Review infinity stone 1:1	review infinity stone glove marvel	3
+9	video cinematic	cara membuat video cinematic	3
+\.
+
+
+--
+-- Name: channel_subscribers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: tatangdev
+--
+
+SELECT pg_catalog.setval('public.channel_subscribers_id_seq', 7, true);
+
+
+--
+-- Name: channels_id_seq; Type: SEQUENCE SET; Schema: public; Owner: tatangdev
+--
+
+SELECT pg_catalog.setval('public.channels_id_seq', 3, true);
+
+
+--
+-- Name: comments_id_seq; Type: SEQUENCE SET; Schema: public; Owner: tatangdev
+--
+
+SELECT pg_catalog.setval('public.comments_id_seq', 30, true);
+
+
+--
+-- Name: likes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: tatangdev
+--
+
+SELECT pg_catalog.setval('public.likes_id_seq', 66, true);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: tatangdev
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 6, true);
+
+
+--
+-- Name: videos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: tatangdev
+--
+
+SELECT pg_catalog.setval('public.videos_id_seq', 9, true);
+
+
+--
+-- Name: channel_subscribers channel_subscribers_pkey; Type: CONSTRAINT; Schema: public; Owner: tatangdev
+--
+
+ALTER TABLE ONLY public.channel_subscribers
+    ADD CONSTRAINT channel_subscribers_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: channels channels_pkey; Type: CONSTRAINT; Schema: public; Owner: tatangdev
+--
+
+ALTER TABLE ONLY public.channels
+    ADD CONSTRAINT channels_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: channels channels_user_id_unique; Type: CONSTRAINT; Schema: public; Owner: tatangdev
+--
+
+ALTER TABLE ONLY public.channels
+    ADD CONSTRAINT channels_user_id_unique UNIQUE (user_id);
+
+
+--
+-- Name: comments comments_pkey; Type: CONSTRAINT; Schema: public; Owner: tatangdev
+--
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT comments_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: likes likes_pkey; Type: CONSTRAINT; Schema: public; Owner: tatangdev
+--
+
+ALTER TABLE ONLY public.likes
+    ADD CONSTRAINT likes_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_email_unique; Type: CONSTRAINT; Schema: public; Owner: tatangdev
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_unique UNIQUE (email);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: tatangdev
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: videos videos_pkey; Type: CONSTRAINT; Schema: public; Owner: tatangdev
+--
+
+ALTER TABLE ONLY public.videos
+    ADD CONSTRAINT videos_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
