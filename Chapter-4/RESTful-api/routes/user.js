0b2674c81df836f1db9ba15ba1028d3dee72fa47
tatang <tatang.romadhona@rushowl.sg>
2022-09-16 21:52:06 +0700

    feat: restful api

--- Chapter-4/RESTful-api/routes/user.js
+++ Chapter-4/RESTful-api/routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers');
 
-router.get('/', controller.user.getAll);
+router.get('/', controller.user.index);
+router.get('/:userId', controller.user.show);
+router.post('/', controller.user.create);
 
 module.exports = router;
