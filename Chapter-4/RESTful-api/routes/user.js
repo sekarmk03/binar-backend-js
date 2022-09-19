4d93e67cbdc25c24b381f12d0245e0a6c9d817e0
Daniel Manurung <danielmanurung79@gmail.com>
2022-09-19 18:01:21 +0700

    menambahkan fitur delete dan crud produk

--- Chapter-4/RESTful-api/routes/user.js
+++ Chapter-4/RESTful-api/routes/user.js
@@ -6,5 +6,6 @@ router.get('/', controller.user.index);
 router.get('/:userId', controller.user.show);
 router.post('/', controller.user.create);
 router.put('/:userId', controller.user.update);
+router.delete('/:userId', controller.user.delete);
 
 module.exports = router;
