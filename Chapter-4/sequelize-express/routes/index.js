39c7e507fe2e17212417e4021d6fd55dfd9d7619
tatang <tatang.romadhona@rushowl.sg>
2022-09-21 21:25:49 +0700

    feat: add middleware

--- Chapter-4/sequelize-express/routes/index.js
+++ Chapter-4/sequelize-express/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const c = require('../controllers');
+const mid = require('../helpers/middleware');
 
 router.post('/auth/register', c.auth.register);
 router.post('/auth/login', c.auth.login);
+router.get('/auth/whoami', mid.mustLogin, c.auth.whoami);
 
 module.exports = router;
