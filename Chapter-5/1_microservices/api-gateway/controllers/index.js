510328c60561c64e017f949897d11d0bdf54f561
tatang <tatang.romadhona@rushowl.sg>
2022-09-26 22:41:59 +0700

    feat: login endpoint

--- Chapter-5/1_microservices/api-gateway/controllers/index.js
+++ Chapter-5/1_microservices/api-gateway/controllers/index.js
@@ -1,6 +1,9 @@
 const adapter = require('../external/apiadapter');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const {
-    USER_SERVICE_HOST
+    USER_SERVICE_HOST,
+    JWT_SECRET_KEY = 'hahaha'
 } = process.env;
 
 const api = adapter(USER_SERVICE_HOST);
@@ -9,39 +12,64 @@ module.exports = {
     register: async (req, res, next) => {
         try {
             const { name, email, password } = req.body;
-            const { status, data } = await api.post('/create', { name, email, password });
+            const { data } = await api.post('/create', { name, email, password });
 
-            if (!data.data.status) {
-                return res.status(status).json({
-                    status: data.data.status,
-                    message: data.data.message,
-                    data: data.data.data
-                });
-            }
-
-            res.status(201).json({
+            return res.status(201).json({
                 status: true,
                 message: 'success',
-                data: response.data.data
+                data: data.data
             });
         } catch (err) {
-            const { status, data } = err.response;
-            if (!status) {
-                next(err);
+            if (err.code == 'ECONNREFUSED') {
+                err = new Error('service anvailable!');
+                return next(err);
             }
-            res.status(status).json(data);
+
+            if (err.response) {
+                const { status, data } = err.response;
+                res.status(status).json(data);
+            }
+
+            next(err);
         }
     },
 
     login: async (req, res, next) => {
         try {
+            const { email, password } = req.body;
+            const { data } = await api.post('/find', { email });
+
+            const user = data.data;
+            const valid = await bcrypt.compare(password, user.password);
+            if (!valid) {
+                return res.status(400).json({
+                    status: false,
+                    message: 'wrong password!',
+                    data: null
+                });
+            }
+
+            const token = jwt.sign(user, JWT_SECRET_KEY);
+            return res.status(200).json({
+                status: true,
+                message: 'success',
+                data: { token }
+            });
 
         } catch (err) {
-            const { status, data } = err.response;
-            if (!status) {
-                next(err);
+            // handle error service unavailable
+            if (err.code == 'ECONNREFUSED') {
+                err = new Error('service anvailable!');
+                return next(err);
             }
-            res.status(status).json(data);
+
+            // handle error dari service lain
+            if (err.response) {
+                const { status, data } = err.response;
+                res.status(status).json(data);
+            }
+
+            next(err);
         }
     }
 };
--- Chapter-5/1_microservices/api-gateway/routes/index.js
+++ Chapter-5/1_microservices/api-gateway/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const c = require('../controllers');
 
 router.post('/auth/register', c.register);
+router.post('/auth/login', c.login);
 
 module.exports = router;
