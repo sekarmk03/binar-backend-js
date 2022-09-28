414a8cfe02b297a6317c19513e297dbc1158d947
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2022-09-28 21:50:57 +0700

    Add routes mentor

--- Chapter-5/2_latihan_microservices/kelompok_3/kelas-service/routes/index.js
+++ Chapter-5/2_latihan_microservices/kelompok_3/kelas-service/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const c = require('../controllers');
 const kelas_user = require('./kelas_user');
 const kelas = require('./kelas');
+const mentor = require('./mentor');
 
 router.use('/kelas_user', kelas_user);
 router.use('/kelas', kelas);
+router.use('/mentor', mentor);
 
 // router.post('/find', c.);
 // router.post('/create', c.create);
