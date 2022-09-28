c5bc10c07390630aa17e8686d6c569966fb9ab08
Otniel Kevin Abiel <otnielkevin.ok@gmail.com>
2022-09-28 21:31:55 +0700

    add kelas

---
+++ Chapter-5/2_latihan_microservices/kelompok_3/kelas-service/controllers/kelas.js
@@ -0,0 +1,80 @@
+const { Kelas } = require("../db/models");
+const kelas = require("../db/models/kelas");
+
+module.exports = {
+  register: async (req, res, next) => {
+    try {
+      const { nama, deskripsi, mentor_id, level } = req.body;
+
+      const exist = await Kelas.findOne({ where: { nama } });
+      if (exist) {
+        return res.status(400).json({
+          status: false,
+          message: "email already used!",
+          data: null,
+        });
+      }
+
+      const user = await kelas.create({
+        nama,
+        deskripsi,
+        mentor_id,
+        level,
+      });
+
+      return res.status(201).json({
+        status: true,
+        message: "success",
+        data: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          pekerjaan: user.pekerjaan,
+        },
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+  findOne: async (req, res, next) => {
+    try {
+      const { nama, mentor_id } = req.body;
+      const exist = await Kelas.findOne({ where: { nama } });
+      if (exist) {
+        return res.status(400).json({
+          status: false,
+          message: "email already used!",
+          data: null,
+        });
+      }
+
+      return res.status(200).json({
+        status: false,
+        message: "success",
+        data: user,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+  findAll: async (req, res, next) => {
+    try {
+      const exist = await Kelas.findAll();
+      if (!exist) {
+        return res.status(400).json({
+          status: false,
+          message: "not found",
+          data: null,
+        });
+      }
+
+      return res.status(200).json({
+        status: false,
+        message: "success",
+        data: user,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+};
--- Chapter-5/2_latihan_microservices/kelompok_3/kelas-service/routes/index.js
+++ Chapter-5/2_latihan_microservices/kelompok_3/kelas-service/routes/index.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const c = require('../controllers');
+const c = require("../controllers");
 
-router.post('/find', c.findOne);
-router.post('/create', c.create);
-router.get('/test', (req, res) => {
-    return res.status(400).json({
-        status: 'ok'
-    });
+router.post("/find", c.findOne);
+router.post("/create", c.create);
+router.get("/test", (req, res) => {
+  return res.status(400).json({
+    status: "ok",
+  });
 });
+
+router.get("/kelas-getAll", c.kelas.findAll);
+router.get("/kelas-get", c.kelas.findOne);
+router.post("/register-kelas", c.kelas.register);
 
 module.exports = router;
