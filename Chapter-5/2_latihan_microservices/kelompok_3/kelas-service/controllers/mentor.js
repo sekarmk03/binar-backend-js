61956c213eb4a5336f578bb135a5e27c913de187
Febi Ago Jonata <febiago7@gmail.com>
2022-09-28 21:57:43 +0700

    Update

--- Chapter-5/2_latihan_microservices/kelompok_3/kelas-service/controllers/mentor.js
+++ Chapter-5/2_latihan_microservices/kelompok_3/kelas-service/controllers/mentor.js
@@ -1,9 +1,7 @@
 const { Mentor } = require("../db/models");
-const bcrypt = require("bcrypt");
 const { response } = require("express");
 
 module.exports = {
-<<<<<<< HEAD
 
     view: async (req, res, next) => {
         const mentor = Mentor.findAll({raw:true});
@@ -34,29 +32,6 @@ module.exports = {
                     data: null
                 });
             }
-=======
-  findOne: async (req, res, next) => {
-    try {
-      const where = {};
-      const { id, nama, pekerjaan } = req.body;
-      if (id) {
-        where.id = id;
-      }
-      if (nama) {
-        where.nama = nama;
-      }
-      if (pekerjaan) {
-        where.pekerjaan = pekerjaan;
-      }
-      const mentor = await Mentor.findOne({ where });
-      if (!mentor) {
-        return req.status(400).json({
-          status: false,
-          message: "not found!",
-          data: null,
-        });
-      }
->>>>>>> 3fa9f836eafb62f34a9c2fde73455a78db466e20
 
       return res.status(200).json({
         status: true,
