5cee32840e9e1b88a4012fde4f08eeb4bec87c81
tatang <tatang.romadhona@rushowl.sg>
2022-09-30 19:01:51 +0700

    feat: apply middleware

--- Chapter-5/2_latihan_microservices/tatangdev/user-service/routes/index.js
+++ Chapter-5/2_latihan_microservices/tatangdev/user-service/routes/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const h = require('../handlers');
+const utils = require('../utils');
 
 router.post('/auth', h.auth.register);
 router.post('/auth', h.auth.login);
 
-router.post('/mentor', h.mentor.create);
-router.get('/mentor', h.mentor.index);
-router.get('/mentor/:mentor_id', h.mentor.show);
+router.post('/mentor', utils.middleware.authUser, h.mentor.create);
+router.get('/mentor', utils.middleware.authUser, h.mentor.index);
+router.get('/mentor/:mentor_id', utils.middleware.authUser, h.mentor.show);
 
-router.post('/course', h.course.create);
-router.get('/course', h.course.index);
-router.get('/course/:course_id', h.course.show);
-router.post('/course/subscribe', h.userCourse.create);
+router.post('/course', utils.middleware.authUser, h.course.create);
+router.get('/course', utils.middleware.authUser, h.course.index);
+router.get('/course/:course_id', utils.middleware.authUser, h.course.show);
+router.post('/course/subscribe', utils.middleware.authUser, h.userCourse.create);
 
 module.exports = router;
