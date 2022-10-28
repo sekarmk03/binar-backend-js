2e61e1eaa2b5d3a329bafc71bce0accf0ac1f4fe
tatang <tatang.romadhona@rushowl.sg>
2022-10-28 22:06:37 +0700

    feat: implement oauth2

--- Chapter-7/3_google_oauth2/utils/oauth2/facebook.js
+++ Chapter-7/3_google_oauth2/utils/oauth2/facebook.js
@@ -13,5 +13,33 @@ module.exports = {
         });
 
         return `https://www.facebook.com/v15.0/dialog/oauth?${params}`;
+    },
+
+    getAccessToken: async (code) => {
+        const { data } = await axios({
+            url: 'https://graph.facebook.com/v15.0/oauth/access_token',
+            method: 'get',
+            params: {
+                client_id: process.env.FB_APP_ID,
+                client_secret: process.env.FB_APP_SECRET,
+                redirect_uri: process.env.FB_REDIRECT_URI,
+                code,
+            },
+        });
+
+        return data.access_token;
+    },
+
+    getUserInfo: async (accesstoken) => {
+        const { data } = await axios({
+            url: 'https://graph.facebook.com/me',
+            method: 'get',
+            params: {
+                fields: ['id', 'email', 'first_name', 'last_name'].join(','),
+                access_token: accesstoken,
+            },
+        });
+
+        return data;
     }
 };
